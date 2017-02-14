ad7d712bcbadf7f86220240e906173bef92580ca
McBen <viertelvor12@gmx.net>
2017-02-14 13:00:19 +0100

    copy script now uses profiles

--- copybuild2firefox.rb
+++ copybuild2firefox.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 begin
-    require 'fileutils'  
+    require 'fileutils'
     require 'os'         # gem install os
     require 'inifile'    # gem install inifile
     require 'nokogiri'   # gem install nokogiri  # may require ruby-dev zlib1c-dev
@@ -13,14 +13,21 @@ class Firefox
 
     attr_reader :scripts
 
-    private_class_method :new
-
-    def self.create()
-        ff = new
+    def self.create(profile=nil)
+        ff = Firefox.new
         if ff.isInstalled? then
-            ff.loadListOfScripts
             return ff
         end
+    end
+
+    def initialize()
+        @firefox_profile_path = File.join(basepath, getProfilePath())
+    end
+
+    def selectProfile(name)
+        @use_profile = name
+        @firefox_profile_path = File.join(basepath, getProfilePath())
+        loadListOfScripts()
     end
 
     def basepath
@@ -31,13 +38,40 @@ class Firefox
         return File.expand_path('~/.mozilla/firefox')
     end
 
-    def firefox_profile_path()
+    private
+    def getProfilePath()
+        inifile = IniFile.load(File.join(basepath,'profiles.ini'))
+
+        if @use_profile && not(inifile[@use_profile].empty?) then
+            return inifile[@use_profile]['Path']
+        end
+
+        profile_name = findProfile(inifile, @use_profile)
+
+        return (inifile[profile_name] && inifile[profile_name]['Path'])
+    end
+
+    private
+    def findProfile(inifile, name)
+        name = name || 'default'
+        inifile.each_section {|section|
+            return section if inifile[section]['Name']==name
+        }
+    end
+
+    public
+    def getListOfProfiles()
         file = IniFile.load(File.join(basepath,'profiles.ini'))
-        return File.join(basepath,file['Profile0']['Path'])
+        profiles=[]
+        file.each_section {|section|
+            profiles.push( file[section]['Name'] ) unless file[section]['Name'].nil?
+        }
+
+        return profiles
     end
 
     def getGreaseMonkyConfigFilename()
-        File.join(firefox_profile_path,'gm_scripts','config.xml')
+        File.join(@firefox_profile_path,'gm_scripts','config.xml')
     end
 
     def isInstalled?
@@ -72,14 +106,36 @@ class Firefox
             return
         end
 
-        FileUtils.cp(src, File.join(firefox_profile_path,'gm_scripts',script[:basedir],script[:filename]))
+        FileUtils.cp(src, File.join(@firefox_profile_path,'gm_scripts',script[:basedir],script[:filename]))
     end
 end
+
+
+
+def main()
+    ff = Firefox.create("dev-edition-default")
+    raise "firefox not found" if ff.nil?
+
+    profiles = getProfiles(ff)
+    raise "GreaseMonky not found" if profiles.empty?
+
+    profiles.each {|pro|
+        puts "Profile: #{pro}" if profiles.size>1
+        ff.selectProfile(pro)
+
+        Dir["build/local/plugins/*.user.js"].each { |filename|
+            updateScript(filename, ff)
+        }
+        updateScript("build/local/total-conversion-build.user.js", ff)
+    }
+end
+
 
 def getPluginName(filename)
     ma = /^\s*\/\/\s*@name\s+(.*)\s*$/.match File.read(filename)
     return (ma and ma[1])
 end
+
 
 def updateScript(filename, browser)
     name = getPluginName(filename)
@@ -89,12 +145,14 @@ def updateScript(filename, browser)
 end
 
 
+def getProfiles(ff)
+    profiles = ff.getListOfProfiles()
+    profiles.delete_if {|pro|
+        ff.selectProfile(pro)
+        !ff.isGreaseMonkyInstalled?
+    }
+    return profiles
+end
 
-ff = Firefox.create()
-raise "firefox+GreaseMonky not found" if ff.nil?
 
-Dir["build/local/plugins/*.user.js"].each { |filename|
-    updateScript(filename, ff)
-}
-updateScript("build/local/total-conversion-build.user.js", ff)
-
+main()
