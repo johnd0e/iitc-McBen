1a72290efd575c1f73f88c97d42411b4c6383d7f
McBen <viertelvor12@gmx.net>
2017-02-24 20:01:22 +0100

    [external] leaflet.draw fixed polygon edit

--- external/leaflet.draw.js
+++ external/leaflet.draw.js
@@ -2104,6 +2104,36 @@ L.GeodesicPolyline.addInitHook(function () {
 });
 
 
+L.GeodesicPolygon.addInitHook(function () {
+
+	// Check to see if handler has already been initialized. This is to support versions of Leaflet that still have L.Handler.PolyEdit
+	if (this.editing) {
+		return;
+	}
+
+	if (L.Edit.Poly) {
+
+		this.editing = new L.Edit.Poly(this, this.options.poly);
+
+		if (this.options.editable) {
+			this.editing.enable();
+		}
+	}
+
+	this.on('add', function () {
+		if (this.editing && this.editing.enabled()) {
+			this.editing.addHooks();
+		}
+	});
+
+	this.on('remove', function () {
+		if (this.editing && this.editing.enabled()) {
+			this.editing.removeHooks();
+		}
+	});
+});
+
+
 
 L.Edit = L.Edit || {};
 /**
